492a9477179c00c54222e6467b81e9f122d25483
daniboygg <danigayosog@gmail.com>
2025-11-24 20:50:38 +0100

    spike for files

--- main.c
+++ main.c
@@ -1,285 +1,74 @@
 #include <stdio.h>
-#include <assert.h>
 #include <string.h>
-
-// coordinate
-typedef struct Coordinate {
-    int x;
-    int y;
-    int z;
-} coordinate_t;
-
-coordinate_t coord_new(int x, int y, int z) {
-    coordinate_t coord = {.x = x, .y = y, .z = z};
-    return coord;
-}
-
-coordinate_t coord_scale(coordinate_t coord, int scale) {
-    coordinate_t c = {.x = coord.x * scale, .y = coord.y * scale, .z = coord.z * scale};
-    return c;
-}
-
-void coord_update(coordinate_t coord, int new_x) {
-    coord.x = new_x;
-}
-
-coordinate_t coord_update_and_return_x(coordinate_t coord, int new_x) {
-    coord.x = new_x;
-    return coord;
-}
-
-// alignment
-typedef struct {
-    char a;
-    char b;
-    char c;
-    char d;
-    int x;
-    int y;
-    int z;
-} coordinate_t_aligned;
+#include <errno.h>
 
 typedef struct {
-    char a;
-    int x;
-    int y;
-    int z;
-} coordinate_t_not_aligned;
+    int len;
+    const char *data;
+} String;
 
-
-// ch3
-typedef struct CodeFile {
-    int lines;
-    int filetype;
-} codefile_t;
-
-codefile_t codefile_change_filetype(codefile_t *cf, int new_filetype) {
-    cf->filetype = new_filetype;
-    return *cf;
+String string_new(const char *str) {
+    String s = {.data = str, .len = strlen(str)};
+    return s;
 }
 
-void update_file(int filedata[200], int new_filetype, int new_num_lines) {
-    filedata[0] = new_num_lines;
-    filedata[1] = new_filetype;
-    filedata[199] = 0;
+size_t string_write(FILE *file, String str) {
+    size_t written = fwrite(str.data, sizeof(char), str.len, file);
+    return written;
 }
-
-typedef struct Graphics {
-    int fps;
-    int height;
-    int width;
-} graphics_t;
-
-void dump_graphics(graphics_t gsettings[10]) {
-    int *ptr = (int *) gsettings;
-    for (int i = 0; i < 30; i++) {
-        printf("settings[%d] = %d\n", i, ptr[i]);
-    }
-}
-
-// void core_utils_func(int core_utilization[]) {
-//     // array decays to a pointer so sizeof return the size of a pointer not size of the array
-//     printf("sizeof core_utilization in core_utils_func: %zu\n", sizeof(core_utilization));
-// }
-
-// L14
-void concat_strings(char *str1, const char *str2) {
-    int end = 0;
-    for (end = 0; str1[end] != '\0'; end++) {
-    }
-    for (int i = 0; str2[i] != '\0'; i++) {
-        str1[end] = str2[i];
-        end++;
-    }
-    str1[end] = '\0';
-}
-
-// CH3 L15
-typedef struct TextBuffer {
-    size_t length;
-    char buffer[64];
-} text_buffer_t;
-
-int smart_append(text_buffer_t *dest, const char *src) {
-    if (dest == NULL || src == NULL) {
-        return 1;
-    }
-    const size_t BUFFER_SIZE = sizeof(dest->buffer);
-    const size_t SCR_LENGTH = strlen(src);
-    const size_t REMAINING = BUFFER_SIZE - dest->length - 1;
-
-    if (SCR_LENGTH > REMAINING) {
-        strncat(dest->buffer, src, REMAINING);
-        dest->length = BUFFER_SIZE - 1;
-        return 1;
-    } else {
-        strncat(dest->buffer, src, REMAINING);
-        dest->length += SCR_LENGTH;
-        return 0;
-    }
-}
-
-//ch4 l1, l2
-typedef enum Color {
-    RED = 55,
-    GREEN = 178,
-    BLUE = 38,
-} color_t;
-
-// ch4 l3
-typedef enum HttpErrorCode {
-    HTTP_BAD_REQUEST = 400,
-    HTTP_UNAUTHORIZED = 401,
-    HTTP_NOT_FOUND = 404,
-    HTTP_TEAPOT = 418,
-    HTTP_INTERNAL_SERVER_ERROR = 500
-} http_error_code_t;
-
-char *http_to_str(http_error_code_t code) {
-    switch (code) {
-        case HTTP_BAD_REQUEST:
-            return "400 Bad Request";
-        case HTTP_UNAUTHORIZED:
-            return "401 Unauthorized";
-        case HTTP_NOT_FOUND:
-            return "404 Not Found";
-        case HTTP_TEAPOT:
-            return "418 I AM A TEAPOT!";
-        case HTTP_INTERNAL_SERVER_ERROR:
-            return "500 Internal Server Error";
-        default:
-            return "Unknown HTTP status code";
-    }
-}
-
-// ch4 l4
-typedef enum {
-    BIG = 123412341234,
-    BIGGER,
-    BIGGEST,
-} big_number_t;
 
 
 int main() {
-    coordinate_t c = coord_new(20, 40, 5);
-    assert(c.x == 20);
-    assert(c.y == 40);
-    assert(c.z == 5);
+    FILE *file = fopen("example.txt", "w");
+    if(file == NULL) {
+        printf("Error getting file %d\n", errno);
+        return -1;
+    }
 
-    coordinate_t scaled = coord_scale(c, 2);
+    String str = string_new("content áaaamigo to show the proper handling of file\n");
+    size_t written = string_write(file, str);
+    printf("written: %zd items of char\n", written);
 
-    assert(scaled.x == 40);
-    assert(scaled.y == 80);
-    assert(scaled.z == 10);
+    if (fclose(file) != 0) {
+        printf("Error closing file %d\n", errno);
+        return -1;
+    }
 
-    assert(sizeof(coordinate_t) == 12);
-    assert(sizeof(coordinate_t_aligned) == 16);
-    assert(sizeof(coordinate_t_not_aligned) == 16);
-
-    // ch3
-    coordinate_t c1 = coord_new(60, 40, 5);
-    coord_update(c1, 20);
-    assert(c1.x == 60);
-
-    c1 = coord_new(60, 40, 5);
-    coordinate_t c2 = coord_update_and_return_x(c1, 20);
-    assert(c1.x == 60);
-    assert(c2.x == 20);
-
-    codefile_t cf = {.lines = 300, .filetype = 2};
-    codefile_change_filetype(&cf, 3);
-    assert(cf.lines == 300);
-    assert(cf.filetype == 3);
-
-    int filedata[200] = {0};
-    update_file(filedata, 2, 2);
-    assert(filedata[0] == 2);
-    assert(filedata[1] == 2);
-    assert(filedata[199] == 0);
-
-    // graphics_t graphics_array[10] = {
-    //     {60, 1080, 1920},
-    //     {30, 720, 1280},
-    //     {144, 1440, 2560},
-    //     {75, 900, 1600},
-    //     {120, 1080, 1920},
-    //     {60, 2160, 3840},
-    //     {240, 1080, 1920},
-    //     {60, 768, 1366},
-    //     {165, 1440, 2560},
-    //     {90, 1200, 1920}
-    // };
-    // dump_graphics(graphics_array);
-
-    // int core_utilization[] = {43, 67, 89, 92, 71, 43, 56, 12};
-    // int len = sizeof(core_utilization) / sizeof(core_utilization[0]);
-    // printf("sizeof core_utilization in main: %zd\n", sizeof(core_utilization));
-    // printf("len of core_utilization: %d\n", len);
-    // core_utils_func(core_utilization);
-
-    do {
-        char str1[100] = "Hello ";
-        const char *str2 = "World";
-        concat_strings(str1, str2);
-        assert(strcmp(str1, "Hello World") == 0);
-        assert(str1[11] == '\0');
-    } while (0);
-
-    // CH3 l15
-    do {
-        text_buffer_t dest;
-        const char *src = NULL;
-        assert(smart_append(&dest, src) == 1);
-    } while (0);
-    do {
-        text_buffer_t dest;
-        strcpy(dest.buffer, "");
-        dest.length = 0;
-        const char *src = "Hello";
-        assert(smart_append(&dest, src) == 0);
-        assert(strcmp(dest.buffer, "Hello") == 0);
-        assert(dest.length == 5);
-    } while (0);
-    do {
-        text_buffer_t dest;
-        strcpy(dest.buffer, "This is a very long string that will fill up the entire buffer.");
-        dest.length = 63;
-        const char *src = " Extra";
-        assert(smart_append(&dest, src) == 1);
-        assert(strcmp(dest.buffer, "This is a very long string that will fill up the entire buffer.") == 0);
-        assert(dest.length == 63);
-    } while (0);
-    do {
-        text_buffer_t dest;
-        strcpy(dest.buffer, "This is a long string");
-        dest.length = 21;
-        const char *src = " that will fill the whole buffer and leave no space for some of the chars.";
-        assert(smart_append(&dest, src) == 1);
-        assert(strcmp(dest.buffer, "This is a long string that will fill the whole buffer and leave") == 0);
-        assert(dest.length == 63);
-    } while (0);
-
-    // ch4 l1, l2
-    assert(RED == 55);
-    assert(GREEN == 178);
-    assert(BLUE == 38);
-    assert(RED != 4);
-    assert(GREEN != 2);
-    assert(BLUE != 0);
-
-    // ch4 l3
-    assert(strcmp(http_to_str(HTTP_BAD_REQUEST), "400 Bad Request") == 0);
-    assert(strcmp(http_to_str(HTTP_UNAUTHORIZED), "401 Unauthorized") == 0);
-    assert(strcmp(http_to_str(HTTP_NOT_FOUND), "404 Not Found") == 0);
-    assert(strcmp(http_to_str(HTTP_TEAPOT), "418 I AM A TEAPOT!") == 0);
-    assert(strcmp(http_to_str(HTTP_INTERNAL_SERVER_ERROR), "500 Internal Server Error") == 0);
-    assert(strcmp(http_to_str((http_error_code_t) 999), "Unknown HTTP status code") == 0);
-
-    // ch4 l4
-    // printf("size of http error : %zu\n", sizeof(http_error_code_t));
-    // printf("size of big numbers: %zu\n", sizeof(big_number_t));
-
-    // ch5 l1
+    
+    FILE *file2 = fopen("example.txt", "r");
+    if(file2 == NULL) {
+        printf("Error getting file %d\n", errno);
+        return -1;
+    }
+    
+    #define buffer_size 9
+    char buffer[buffer_size] = {0};
+    while(1) {
+        size_t read = fread(buffer, sizeof(char), buffer_size - 1, file2);
+        if (read == 0) break;
+        buffer[read] = '\0';
+        printf("%s", buffer);
+    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
